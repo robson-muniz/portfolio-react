f69e65c0240d2b264bc4ac18059004fe5576b82d
robson-muniz <robson_muniz@yahoo.com>
2025-09-26 18:12:16 +0100

    Animated foot

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import Hero from "./components/Hero.jsx";
 import Projects from "./components/Projects.jsx";
 import Contact from "./components/Contact.jsx";
 import {useEffect, useState} from "react";
+import emailjs from "@emailjs/browser";
+import {motion} from "framer-motion";
 
 function App() {
     const [isLoaded, setIsLoaded] = useState(false)
 
     useEffect(() => {
         setIsLoaded(true)
+        emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
     }, [])
     
     return <div className={`App ${isLoaded ? "loaded" : ""}`}>
@@ -19,9 +22,15 @@ function App() {
         <Projects />
         <Contact />
 
-        <>
+        <motion.footer
+            className='footer'
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{once: true}}
+            transition={{duration: 0.6}}
+        />
             <p>&copy; Robson Muniz. All rights reserved.</p>
-        </>
+        </motion.footer>
     </div>
 }
 
